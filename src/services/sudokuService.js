a79197c907aae039f9a0cc71d68da28dc37af11d
Flavio Sousa <flaviosousa@cvccorp.com.br>
2021-09-05 21:38:51 -0300

    remove compare models

--- src/services/sudokuService.js
+++ src/services/sudokuService.js
@@ -18,14 +18,6 @@ function _modelToString(model) {
   );
 }
 
-function _modelsEquals(m1, m2) {
-  let i = m1.length;
-  while (--i) {
-    if (m1.charAt(i) !== "." && m1.charAt(i) !== m2.charAt(i)) return false;
-  }
-  return true;
-}
-
 function _setCharAt(str, index, chr) {
   if (index > str.length - 1) return str;
   return str.substring(0, index) + chr + str.substring(index + 1);
