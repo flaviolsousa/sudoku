1e290fa22fec85faf6f0c6fd8e33998a8e614726
Flavio Sousa <flaviosousa@cvccorp.com.br>
2021-09-19 17:11:53 -0300

    Init background

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ContentApp from "./components/ContentApp/ContentApp";
 import { MemoryRouter as Router, Route } from "react-router-dom";
 import SplashScreen from "./components/SplashScreen/SplashScreen";
 import PageTransition from "./components/PageTransition/PageTransition";
+import BackgroundApp from "./components/BackgroundApp/BackgroundApp";
 
 import "./assets/style.css";
 
@@ -14,19 +15,21 @@ const routes = [
 function App() {
   return (
     <div className="App">
-      <ContentApp>
-        <Router>
-          {routes.map(({ path, Component }) => (
-            <Route key={path} exact path={path}>
-              {({ match }) => (
-                <PageTransition match={match}>
-                  <Component />
-                </PageTransition>
-              )}
-            </Route>
-          ))}
-        </Router>
-      </ContentApp>
+      <BackgroundApp>
+        <ContentApp>
+          <Router>
+            {routes.map(({ path, Component }) => (
+              <Route key={path} exact path={path}>
+                {({ match }) => (
+                  <PageTransition match={match}>
+                    <Component />
+                  </PageTransition>
+                )}
+              </Route>
+            ))}
+          </Router>
+        </ContentApp>
+      </BackgroundApp>
     </div>
   );
 }
---
+++ src/components/BackgroundApp/BackgroundApp.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.scss";
+
+function BackgroundApp(props) {
+  return (
+    <>
+      <div class="background">
+        {props.children}
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+        <span class="background-item"></span>
+      </div>
+    </>
+  );
+}
+
+export default BackgroundApp;
