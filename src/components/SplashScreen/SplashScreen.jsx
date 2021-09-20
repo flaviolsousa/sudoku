3004c39428b448eb836c6464c86a838bdd1da742
Flavio Sousa <flaviosousa@cvccorp.com.br>
2021-09-19 22:51:19 -0300

    Fix background sass

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
 
+// import Badge from "../../../node_modules/material-kit-react/src/components/Badge/Badge.js";
+
 import "./style.css";
 
 function SplashScreen() {
@@ -11,6 +13,7 @@ function SplashScreen() {
   ) : (
     <div className="SplashScreen">
       <h1>Hello Sudoku</h1>
+      {/* <Badge color="primary">primary</Badge> */}
     </div>
   );
 }
